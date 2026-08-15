0ebf0b6dfd36eb658a9a28461d593bc0f1e2b971
interiorworks-arty <arty.kishi@gmail.com>
2026-08-16 00:21:10 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="gencho-v080-20260816-separate-finish-pages";
+const CACHE="gencho-v081-20260816-page-tabs-fix";
 const ASSETS=["./manifest.webmanifest","./icon.svg","./icon-192.png","./icon-512.png","./apple-touch-icon.png"];
 
 self.addEventListener("install",event=>{
