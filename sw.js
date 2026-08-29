0edb5b06b73c664cdeedfd155ee324f2faa4f7b8
interiorworks-arty <arty.kishi@gmail.com>
2026-08-29 22:22:12 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="gencho-v2428-glue-number-keypad";
+const CACHE="gencho-v2429-keypad-bottom-right-v5";
 const ASSETS=["./manifest.webmanifest","./icon.svg","./icon-192.png","./icon-512.png","./apple-touch-icon.png"];
 
 self.addEventListener("install",event=>{
